7ede6b6cbf4d07cacddd66d98f299d3f5cbc892c
Jinwoo Kim <contact@kimjinwoo.me>
2026-09-17 11:40:02 +0900

    Add Arduino sketch to toggle LED and print count

---
+++ 04_example_3.ino
@@ -0,0 +1,33 @@
+#define PIN_LED 13
+
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+
+  Serial.begin(115200);
+
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+
+  Serial.println("Hello World!");
+
+  count = 0;
+  toggle = 0;
+
+  digitalWrite(PIN_LED, toggle); // LED OFF
+}
+
+void loop() {
+  Serial.println(++count);
+
+  toggle = toggle_state(toggle); // toggle LED value
+  digitalWrite(PIN_LED, toggle); // update LED status
+
+  delay(1000); // wait for 1 second
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
